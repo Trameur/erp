6cbadcba95f86d5d29c8e26158abbed5c8a51b5a
Gaia <Gaia@erp.com>
2018-09-20 15:53:28 +0200

    config

--- main.cc
+++ main.cc
@@ -4,11 +4,22 @@
 using namespace std;
 
 
+void readConfiguration(char *filename)
+{
+    cout << filename << endl;
+}
+
 int main (int argc, char *argv[])
 {   
 
     if(argc < 2)
         cout << "Error usage " << argv[0] << " [filename]" << endl;
+    else
+    {
+        readConfiguration(argv[1]);
+    }
+    
+    
 
 
 
