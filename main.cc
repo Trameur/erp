72e6042513e18fbda562d57a66519cd40898a7c5
Gaia <Gaia@erp.com>
2018-09-20 16:00:52 +0200

    ajout class employee

--- main.cc
+++ main.cc
@@ -3,6 +3,23 @@
 
 using namespace std;
 
+class Employee {
+
+    public:
+        string name;
+        string date;
+        int role; // 0 = dev, 1 = chef
+
+        Employee();
+        Employee(string name, string date, int role)
+        {
+            this->name = name;
+            this->date = date;
+            this->role = role;
+        }
+        ~Employee();
+
+};
 
 void readConfiguration(char *filename)
 {
@@ -13,14 +30,18 @@ int main (int argc, char *argv[])
 {   
 
     if(argc < 2)
-        cout << "Error usage " << argv[0] << " [filename]" << endl;
-    else
     {
-        readConfiguration(argv[1]);
+        cout << "Error usage " << argv[0] << " [filename]" << endl;
+        return 1;
     }
+
+    readConfiguration(argv[1]);
+
+    //Employee emp1("nom", "15/05/19");
     
     
 
+    
 
 
     return 0;
