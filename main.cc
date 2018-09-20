77e793160a8264b1e934ffbdc475d00e68e8c67f
Gaia <Gaia@erp.com>
2018-09-20 15:14:26 +0200

    main.cc

---
+++ main.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<iostream>
+
+using namespace std;
+
+
+int main (int argc, char *argv[])
+{   
+
+    if(argc < 2)
+        cout << "Error usage " << argv[0] << " [filename]" << endl;
+
+
+
+    return 0;
+}
