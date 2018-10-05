0c9f3d57855031372b884cc726c2c8e970956b71
Gaia <Gaia@erp.com>
2018-10-05 16:26:16 +0200

    avant dernier commit final pour le ping pong

--- main.cc
+++ main.cc
@@ -1,6 +1,8 @@
 #include <stdio.h>
-#include<iostream>
-
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <ctime>
 using namespace std;
 
 class Employee {
@@ -26,31 +28,110 @@ class Projet {
 
     public:
         string name;
-        string date;
+        int jour, mois, annee;
         int dev;
         int gestion;
 
         Projet();
 
-        Projet(string name, string date, int dev, int gestion)
+        Projet(string name, int dev, int gestion, int jour, int mois, int annee)
         {
             this->name = name;
-            this->date = date;
             this->dev = dev;
             this->gestion = gestion;
+            this->jour = jour;
+            this->mois = mois;
+            this->annee = annee;
         }
 
         ~Projet() { }
 };
 
-void readConfiguration(char *filename)
-{
-    cout << filename << endl;
+vector<Employee> parseEmployee(string path){
+   ifstream fichier(path);
+   string name;
+   string date;
+   int role;
+
+   int dev, projet;
+
+   if(!fichier.eof())
+       fichier >> dev >> projet;
+
+   vector<Employee> tabEmp;
+
+   for(int i=0; i < dev; i++){
+
+       if(!fichier.eof())
+           fichier >> name >> date >> role;
+
+       Employee emp(name, date, role);
+       cout << emp.name << emp.date << emp.role << endl;
+
+       tabEmp.push_back(emp);
+
+   }
+   fichier.close();
+
+   return tabEmp;
+
 }
 
+vector<Projet> parseProject(string path){
+   ifstream fichier(path);
+
+   string name;
+   int dev;
+   int gestion;
+   int projet;
+   int role;
+
+   int jour,mois, annee;
+
+   if(!fichier.eof())
+       fichier >> dev >> projet;
+
+   vector<Projet> tabProjet;
+   string s;
+   for(int i=0; i < dev*3+1; i++){
+
+       if(!fichier.eof())
+           getline(fichier, s);
+   }
+
+   getline(fichier, s);
+
+   for(int i=0; i < projet; i++){
+
+       if(!fichier.eof())
+           fichier >> name >> dev >> gestion >> jour  >> mois >> annee;
+
+       tabProjet.push_back(Projet(name, dev, gestion, jour, mois, annee));
+       cout << name << endl;
+   }
+
+
+
+   fichier.close();
+
+   return tabProjet;
+
+}
+
+void datePlusDays(struct tm* date, int days)
+{
+    const time_t ONE_DAY = 24 * 60 * 60 ;
+
+    // Seconds since start of epoch
+    time_t date_seconds = mktime( date ) + (days * ONE_DAY) ;
+
+    // Update caller's date
+    // Use localtime because mktime converts to UTC so may change date
+    *date = *localtime( &date_seconds ) ; ;
+}
 
 int main (int argc, char *argv[])
-{   
+{
 
     if(argc < 2)
     {
@@ -58,41 +139,74 @@ int main (int argc, char *argv[])
         return 1;
     }
 
-    readConfiguration(argv[1]);
+    vector<Employee> allEmployees = parseEmployee(argv[1]);
 
-    Employee emp1("dev", "01/06/19", 0);
-    Employee chef("chef", "01/06/19", 1);
-
-    Projet airbus("airbus", "01/12/19", 150, 40);
+    vector<Projet> allProjects = parseProject(argv[1]);
 
     int counter = 0;
-    int days = 0;
+    int days = 4;
     bool toutFini = false;
+    int projetDev = 0;
+    int projetChef = 0;
 
     while(!toutFini)
     {
+
         if(days < 5)
         {
-            if(airbus.dev > 0)
-                airbus.dev--;
+          for(auto it = allEmployees.begin(); it != allEmployees.end() ; it++)
+          {
 
-            if(airbus.gestion > 0)
-                airbus.gestion--;
+            if(it->role == 0)
+            {
+              if(allProjects[projetDev].dev > 0)
+              {
+                allProjects[projetDev].dev--;
+              }
+              else if(allProjects.size() > projetDev)
+              {
+                allProjects[++projetDev].dev--;
+              }
+            }
+            else if(it->role == 1)
+            {
+              if(allProjects[projetChef].gestion > 0)
+              {
+                allProjects[projetChef].gestion--;
+              }
+              else if(allProjects.size() > projetChef)
+              {
+                allProjects[++projetChef].gestion--;
+              }
+            }
+          }
         }
-        
+
         days++;
         if(days > 6)
             days = 0;
 
-        counter++;
-
-        if(airbus.dev == 0 && airbus.gestion == 0)
+        if(allProjects.size() == projetDev && allProjects.size() == projetChef)
         {
             toutFini = true;
         }
+        else
+            counter++;
     }
 
     cout << "Nombre de jours : " << counter << endl;
+    struct tm date = { 0 , 0 , 18 } ;
+    int year = 2018;
+    int month = 6;
+    int day = 1;
+
+    date.tm_year = year - 1900;
+    date.tm_mon = month - 1;
+    date.tm_mday = day;
+
+    datePlusDays(&date, counter);
+
+    cout << "Fin prévue : " << asctime(&date) << endl;
 
 
 
--- parse.cpp
+++ parse.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
@@ -26,50 +27,3 @@ class Employee {
         }
 
 };
-
- void parse(string path, Employee e1, Employee e2,Employee e3,Employee e4){
-    ifstream fichier(path);
-    string name;
-    string date;
-    int role;
-
-    int dev,chef,projet;
-
-    if(!fichier.eof())
-        fichier >> dev >> chef >> projet;
-
-    for(int i=0; i < dev+chef; i++){
-
-        if(!fichier.eof())
-            fichier >> name >> date >> role;
-
-        switch(i){
-        case 0 : e1.name=name;   e1.date = date; e1.role = 1; break;
-        case 1 : e2.name=name;   e2.date = date; e2.role = 0; break;
-        case 2 : e3.name=name;   e3.date = date; e3.role = 0; break;
-        case 3 : e4.name=name;   e4.date = date; e4.role = 0; break;
-        default : break;
-
-        }
-        cout << name << date << role << endl;
-
-    }
-
-
-    fichier.close();
-
-
-}
-
-int main()
-{
-    Employee e1;
-    Employee e2;
-    Employee e3;
-    Employee e4;
-    parse("data",e1,e2,e3,e4);
-
-
-
-    return 0;
-}
